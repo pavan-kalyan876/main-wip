6783f46a41440ead20b1b147bc0205ec30ba5d89
pavansta <pavan.learn69@gmail>
2024-10-17 22:50:22 +0530

    created schema of customer

---
+++ main-project/backend/models/Customer.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const customerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  balance: {
+    type: Number,
+    default: 0,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+});
+const Customer = mongoose.model("Customer", customerSchema);
+module.exports = Customer;
