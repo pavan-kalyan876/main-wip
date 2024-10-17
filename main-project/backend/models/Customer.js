575d2d6662840df520b97cfdc93f849d63bce7e8
pavansta <pavan.learn69@gmail>
2024-10-18 01:34:24 +0530

    updated in user model added admin

--- main-project/backend/models/Customer.js
+++ main-project/backend/models/Customer.js
@@ -5,6 +5,10 @@ const customerSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   isActive: { type: Boolean, default: true },
+  admin: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Admin",
+  },
 });
 
 module.exports = mongoose.model("Customer", customerSchema);
